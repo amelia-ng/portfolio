28b2d9d2abf7ab6c7962ee9e853bc7c4e6b74314
thuyanhnx <ngthuyanh2001@gmail.com>
2026-01-13 10:12:35 -0500

    Update file

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,15 @@
-
 const isProd = process.env.NODE_ENV === "production";
-
-const repoName = "/amelia_portfolio"; 
 
 const nextConfig = {
   reactStrictMode: true,
-  basePath: isProd ? repoName : "",
-  assetPrefix: isProd ? repoName : "",
-  trailingSlash: true,       // Ensures folders like /blog/ become /blog/index.html
+
+  output: "export",
+
+  basePath: isProd ? "/amelia_portfolio" : "",
+  assetPrefix: isProd ? "/amelia_portfolio/" : "",
+
   images: {
-    unoptimized: true,       // Required for static sites on GitHub
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-      };
-    }
-    return config;
+    unoptimized: true,
   },
 };
 
